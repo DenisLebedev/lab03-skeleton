a137e3a939421b3f55d5f2924bfc55c94d856b6d
DenisLebedev <lebedev.denis@hotmail.com>
2017-09-17 14:37:41 -0400

    Override callbacks

--- app/src/main/java/course/labs/activitylab/ActivityOne.java
+++ app/src/main/java/course/labs/activitylab/ActivityOne.java
@@ -11,48 +11,95 @@ import android.widget.TextView;
 public class ActivityOne extends Activity {
 
 		// string for logcat documentation
-		private final static String TAG = "Lab-ActivityOne";
+	private final static String TAG = "Lab-ActivityOne";
     private int createCount, startCount, resumeCount, pauseCount,
             stopCount, restartCount, destroyCount;
     private TextView onCreateTxt, onStartTxt, onResumeTxt,
             onPauseTxt, onStopTxt, onRestartTxt, onDestroyTxt;
 
-		// lifecycle counts
-		//TODO: Create 7 counter variables, each corresponding to a different one of the lifecycle callback methods.
-		//TODO:  increment the variables' values when their corresponding lifecycle methods get called.
+	// lifecycle counts
+	//TODO: Create 7 counter variables, each corresponding to a different one of the lifecycle callback methods.
+	//TODO:  increment the variables' values when their corresponding lifecycle methods get called.
 		
-		@Override
-		protected void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.activity_one);
-            viewInitialization();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_one);
+		viewInitialization();
 
-			//Log cat print out
-			Log.i(TAG, "onCreate called");
-			
-			//TODO: update the appropriate count variable & update the view
-		}
+		//Log cat print out
+		Log.i(TAG, "onCreate called");
+		//String test = getResources().getString(R.string.onCreate);
+		//TODO: update the appropriate count variable & update the view
+		onCreateTxt.setText(getResources().getString(R.string.onCreate) + ++createCount);
+		onStartTxt.setText(getResources().getString(R.string.onStart) + startCount);
+		onResumeTxt.setText(getResources().getString(R.string.onResume) + resumeCount);
+		onPauseTxt.setText(getResources().getString(R.string.onPause) + pauseCount);
+		onStopTxt.setText(getResources().getString(R.string.onStop) + stopCount);
+		onRestartTxt.setText(getResources().getString(R.string.onRestart) + restartCount);
+		onDestroyTxt.setText(getResources().getString(R.string.onDestroy) + destroyCount);
+	}
 
-		@Override
-		public boolean onCreateOptionsMenu(Menu menu) {
-			// Inflate the menu; this adds items to the action bar if it is present.
-			getMenuInflater().inflate(R.menu.activity_one, menu);
-			return true;
-		}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_one, menu);
+		return true;
+	}
 		
-		// lifecycle callback overrides
+	// lifecycle callback overrides
 		
-		@Override
-		public void onStart(){
-			super.onStart();
+	@Override
+	public void onStart(){
+		super.onStart();
 			
-			//Log cat print out
-			Log.i(TAG, "onStart called");
-			
-			//TODO:  update the appropriate count variable & update the view
-		}
+		//Log cat print out
+		Log.i(TAG, "onStart called");
+		onStartTxt.setText(getResources().getString(R.string.onStart) + ++startCount);
+		//TODO:  update the appropriate count variable & update the view
+	}
 
-	    // TODO: implement 5 missing lifecycle callback methods
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		Log.i(TAG, "onResume called");
+		onResumeTxt.setText(getResources().getString(R.string.onResume) + ++resumeCount);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		Log.i(TAG, "onPause called");
+		onPauseTxt.setText(getResources().getString(R.string.onPause) + ++pauseCount);
+
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+
+		Log.i(TAG, "onStop called");
+		onStopTxt.setText(getResources().getString(R.string.onStop) + ++stopCount);
+	}
+
+	@Override
+	protected void onRestart(){
+		super.onRestart();
+
+		Log.i(TAG, "onRestart called");
+		onRestartTxt.setText(getResources().getString(R.string.onRestart) + ++restartCount);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		Log.i(TAG, "onDestroy called");
+		onDestroyTxt.setText(getResources().getString(R.string.onDestroy) + ++destroyCount);
+	}
+
 
 		@Override
 		public void onSaveInstanceState(Bundle savedInstanceState){
